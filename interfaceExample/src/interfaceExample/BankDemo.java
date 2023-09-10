6fb9c84f0113fe602ed0c840cb881e3dd5533256
karishma1507 <115129197+karishma1507@users.noreply.github.com>
2023-09-10 22:24:02 +0530

    interface demo

---
+++ interfaceExample/src/interfaceExample/BankDemo.java
@@ -0,0 +1,14 @@
+package interfaceExample;
+
+public class BankDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Bank ba=new bankimpl();
+		Account acc=new Account(12345,"suma",15500,ba);
+		acc.deposit(acc, 20000);
+		System.out.println(acc);
+		
+	}
+
+}
---
+++ interfaceExample/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module interfaceExample {
+}
