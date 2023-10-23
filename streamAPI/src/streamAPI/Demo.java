977e3854e33af5edfda1d67df60373b0064470b8
Welcome <Welcome@DESKTOP-B64FANO>
2023-10-23 18:13:59 +0530

    streamAPI

---
+++ streamAPI/src/streamAPI/Demo.java
@@ -0,0 +1,12 @@
+package streamAPI;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StreamOperations ob =new StreamOperations();
+		ob.operations();
+	
+	}
+
+}
