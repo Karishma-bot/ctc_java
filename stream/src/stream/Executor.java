ee2bedb5042c5ec459a2ac5211c42fb9b8fb0a6a
Welcome <Welcome@DESKTOP-B64FANO>
2023-10-23 17:59:30 +0530

    streams 2

---
+++ stream/src/stream/Executor.java
@@ -0,0 +1,13 @@
+package stream;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BuiltinFunctionInterface ob=new BuiltinFunctionInterface();
+		ob.printCities();
+		ob.filterCities();
+		ob.supplyCities();
+	}
+
+}
---
+++ stream/src/stream/StreamDemo.java
@@ -0,0 +1,11 @@
+package stream;
+
+public class StreamDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StreamOperations1 ob=new StreamOperations1();
+		ob.allOperations();
+	}
+
+}
